7617c1a503fe95ee0f02e83c3a64cae375d3ab61
MOHD ALMAS HUSAIN <108673527+MOHDALMASHUSAIN@users.noreply.github.com>
2023-04-15 22:23:13 +0530

    Add files via upload

---
+++ StringGFG.java
@@ -0,0 +1,44 @@
+import java .util.*;
+
+public class StringGFG {
+    public static void main(String[] args) {
+        String s = "alams ";
+    //String s = new String("radhe ");
+   // System.out.println(s);
+  // s= s.concat("pintu");
+        StringBuilder p = new StringBuilder("kkk");
+       String b= "idnkmd";
+        System.out.println( s+b);
+        StringBuilder k = new StringBuilder("kkjk");
+k.  append(             "cjnic");
+        System.out.println(k);
+        // alamalamsidknm
+//        skldnfdgnjonksdncscxskdon
+        // kkjkcjnic
+
+      //  String s = new String("almas");
+        String s1 ="kxsmcm";
+
+          char h = s1.charAt(3);
+        System.out.println(h);
+        String str = "GeeksforGeeks is a computer science portal";
+      //int l =   str.length();
+       // System.out.println(str.length());
+       // CharSequence sc = "eeksf";
+
+
+        CharSequence seq = "is a computer ";
+        Boolean bool = str.contains (seq);
+        System.out.println(bool);
+        String stri
+                = "GeeksforGeeks is a computer science portal";
+        int i = stri.indexOf("computer");
+        System.out.println(i);
+        System.out.println(stri.startsWith("computer",19));
+      //  size_t find (const char c, size_t pos = 0);
+    //    */
+
+
+    }
+}
+
