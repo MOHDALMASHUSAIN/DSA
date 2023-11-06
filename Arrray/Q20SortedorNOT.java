6654344652b5910c7a1c55d714a574d1f3f9eefa
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2023-11-06 23:27:59 +0530

    third commit

--- Arrray/Q20SortedorNOT.java
+++ Arrray/Q20SortedorNOT.java
@@ -4,13 +4,21 @@ public class Q20SortedorNOT {
     public static void main(String[] args) {
 
         int arr[] = {12,54,56,233,65,762};
+        boolean flag= true;
+
             for (int i =0;i<arr.length;i++){
                   if(arr[i] >arr[i+1]){
-                  
-                   System.out.println("not sorted");
+                  flag = false;
+                   
                    break;
                }     
             
+            }
+            if(flag){
+                System.out.println("the array is sorted");
+            }
+            else{
+                System.out.println("Array is not sorted");
             }
         }
     }
