3637238ebcc67c9dc64ff9e01d6ea6238783483b
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2024-01-03 16:29:37 +0530

    add files via upload

--- CH10Linkedlist/Class2_Q2Palindrome.java
+++ CH10Linkedlist/Class2_Q2Palindrome.java
@@ -2,6 +2,15 @@ package CH10Linkedlist;
 import java.util.LinkedList;
 import java.util.Scanner;
 public class Class2_Q2Palindrome {
+    static int palindrome( LinkedList<Integer>list){
+        for (int i = 0,j=list.size()-1;i<list.size()/2 ; i++,j--){
+            if (list.get(i)==list.get(j)) {    
+                        
+               return 1;               
+            }          
+        }        
+        return -1;
+    }
     public static void main(String[] args) {
         LinkedList<Integer>list = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
@@ -14,15 +23,15 @@ public class Class2_Q2Palindrome {
             } 
             list.add(element);           
         }
-        for (int i = 0,j=list.size()-1;i<list.size()/2 ; i++,j--){
-            if (list.get(i)!=list.get(j)) {
-                System.out.println("not palindrome");
-                break;
-            }
-           
+      
+        if (palindrome(list)==1) {
+            System.out.println("palindrome");
         }
-         System.out.println("palindrome");
-           sc.close(); 
+        else{
+            System.out.println("not a palindrome");
+        }
+        sc.close();
 
     }
 }
+// check using flag .
