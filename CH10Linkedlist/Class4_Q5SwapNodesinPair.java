328d7fc989dfa5aadbd137175716d9172bb49003
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2024-01-09 22:02:28 +0530

    add files via upload

--- CH10Linkedlist/Class4_Q5SwapNodesinPair.java
+++ CH10Linkedlist/Class4_Q5SwapNodesinPair.java
@@ -3,15 +3,16 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Class4_Q5SwapNodesinPair {
-    static void SwapNodesinPair(LinkedList<Integer>list){
+    static void SwapNodesinPair(LinkedList<Integer>list){     
         for (int i = 0; i < list.size(); i++) {
             int temp = list.get(i);
            list.set(i, list.get(i+1));
            list.set(i+1, temp);
-           i=i+2;
+           i=i+1;
         }
         System.out.println(list);
     }
+    
     public static void main(String[] args) {
         LinkedList<Integer>list1 = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
