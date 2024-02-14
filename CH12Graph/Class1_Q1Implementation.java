dde645d2008f06d6e69bfbfec074824d527fa7fa
MOHD ALMAS HUSAIN <iconalmas@gmail.com>
2024-02-14 12:06:41 +0530

    add files via upload

--- CH12Graph/Class1_Q1Implementation.java
+++ CH12Graph/Class1_Q1Implementation.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import javax.management.Query;
+//import javax.management.Query;
 class Graph {
     private Map<Integer, List<Integer>> g;
 
--- CH12Graph/Class2_Q2BFS.java
+++ CH12Graph/Class2_Q2BFS.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+
 class Graph {
     private static Map<Integer, List<Integer>> g;
 
@@ -39,6 +40,7 @@ class Graph {
             System.out.println();
         }
     }
+
     void updateGraph(int nodeA, int nodeB, boolean flag) {
         if (flag) {
             addEdge(nodeA, nodeB);
@@ -46,6 +48,7 @@ class Graph {
             removeEdge(nodeA, nodeB);
         }
     }
+
     void removeEdge(int nodeA, int nodeB) {
         if (g.containsKey(nodeA) && g.containsKey(nodeB)) {
             g.get(nodeA).remove(Integer.valueOf(nodeB));
@@ -54,6 +57,7 @@ class Graph {
             System.out.println("One or both nodes do not exist");
         }
     }
+
     void deleteNode(int node) {
         if (g.containsKey(node)) {
             g.remove(node);
@@ -64,30 +68,39 @@ class Graph {
             System.out.println("Node does not exist");
         }
     }
+
     static void bfs(int start_node) {
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
-        queue.offer(start_node);
+        queue.add(start_node);
         visited.add(start_node);
 
         while (!queue.isEmpty()) {
             int currentNode = queue.poll();
             System.out.print(currentNode + " ");
 
-            for (int relative : g.get(currentNode)) {
-                if (!visited.contains(relative)) {
-                    visited.add(relative);
-                    queue.offer(relative);
+            for (int neighbor : g.get(currentNode)) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
                 }
             }
         }
     }
 }
+
 public class Class2_Q2BFS {
     public static void main(String[] args) {
         Graph graph = new Graph();
         graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.addEdge(1, 2);
         graph.addEdge(1, 3);
-        graph.print();
+        graph.addEdge(2, 4);
+        
+        System.out.print("BFS Traversal: ");
+        Graph.bfs(1);
     }
 }
